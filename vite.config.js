bf4b4a6a33556f120c3763e5dfdb9301df72c184
18271683093 <18271683093>
2025-05-30 00:47:51 +0800

    核心js工具链重构

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,39 @@
 import { defineConfig } from 'vite';
+import babel from '@rollup/plugin-babel';
 export default defineConfig({
-    plugins: [],
+    plugins: [
+        babel({
+            babelHelpers: 'bundled',
+            presets: [
+                [
+                    '@babel/preset-env',
+                    {
+                        targets: {
+                            browsers: [
+                                "last 2 versions",
+                                "> 1%",
+                                "not dead",
+                                "ie >= 8"
+                            ]
+                        },
+                        useBuiltIns: false,
+                        corejs: 3,
+                    }
+                ]
+            ],
+            exclude: 'node_modules/**' // 只转换源代码
+        })
+    ],
     build: {
-        minify: true,
         lib: {
-            entry: './src/index.js',
+            entry: 'src/index.js',
             name: 'NicePassWord',
-            formats: ["es", 'umd'],
+            formats: ["es", "cjs", 'umd'],
             fileName: (format) => `nice-password.${format}.min.js`
         },
-
+        minify: false,
+    },
+    server: {
+        open: 'src/index.html'
     }
 });
